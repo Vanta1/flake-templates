505240c204b66d2e5e8300d33f6c12f7167aa861
Vanta_1 <mcoopersandys@gmail.com>
2024-06-15 01:41:55 -0400

    hhhhhgghhh finally got it actually working lol

--- my-zola-shell/flake.nix
+++ my-zola-shell/flake.nix
@@ -1,25 +1,23 @@
-# this is mostly from https://fasterthanli.me/series/building-a-rust-service-with-nix/part-10#a-flake-with-derivation
-
-let 
-  name = "my-zola-shell";
-in
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+  outputs = { self, nixpkgs, flake-utils }:
+    let 
+      name = "my-zola-shell";
+    in
     flake-utils.lib.eachDefaultSystem
       (system:
         let
           pkgs = import nixpkgs {
             inherit system;
-            zola
           };
         in
         with pkgs;
         {
           devShells.default = mkShell {
+            packages = [ zola ];
             shellHook = ''
               echo "entering ${name} devshell..."
             '';
